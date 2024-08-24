07fbcfc2a3b30a8509ea969aeb279aeed4807dab
Daniel Giao <daniel.giao@icloud.com>
2024-08-24 14:28:02 -0400

    Added ask route

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from model_hf import model_pipeline
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+import io
+from PIL import Image
 
 app = FastAPI()
 
@@ -8,4 +10,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/ask")
+def ask(text: str, image: UploadFile):
+    content = image.file.read()
+    image = Image.open(io.BytesIO(content))
+    result = model_pipeline(text, image)
+    return {"answer": result}
 
